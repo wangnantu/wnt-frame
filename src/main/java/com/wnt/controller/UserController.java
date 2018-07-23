8a63036176d4476325928b2b7b36b8b07b2ca143
wangnantu <wangnantu@hotmail.com>
2018-07-23 14:13:07 +0800

    1

--- src/main/java/com/wnt/controller/UserController.java
+++ src/main/java/com/wnt/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.wnt.util.JsonUtil;
 import com.wnt.domain.User;
@@ -167,6 +168,14 @@ import com.wnt.util.MD5Util;
 	    	return map;
 	    }
 		
+	    @RequestMapping(value="/show",method=RequestMethod.GET)
+	    public ModelAndView index(){
+	    	ModelAndView modelAndView = new ModelAndView("/html/index");  
+	        modelAndView.addObject("name", "xxx");  
+	        return modelAndView;  
+	    }
+	    
+	    
 	  private Boolean verifyPassword(String inputuserid,String inputpassword){
 		  Boolean pswdOk = false;
 		  User u = this.userService.getUserById(inputuserid);
